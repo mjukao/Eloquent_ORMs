247d1edc22c15660a9a7859b2182d23d110102b3
Search Employee <kittikol57@gmail.com>
2025-02-11 10:31:52 +0700

    q

--- database/migrations/2025_01_30_052328_create_registers_table.php
+++ database/migrations/2025_01_30_052328_create_registers_table.php
@@ -9,25 +9,25 @@ class CreateRegistersTable extends Migration
     /**
      * Run the migrations.
      *
-     * @return void
+     * ฟังก์ชันนี้ใช้ในการสร้างตาราง 'registers' ในฐานข้อมูล
      */
     public function up()
     {
         Schema::create('registers', function (Blueprint $table) {
-            $table->id(); // id ของตาราง pivot
-            $table->foreignId('student_id')->constrained()->onDelete('cascade'); // foreign key สำหรับ student_id
-            $table->foreignId('course_id')->constrained()->onDelete('cascade'); // foreign key สำหรับ course_id
-            $table->timestamps(); // สำหรับการติดตามเวลาเมื่อมีการเพิ่มข้อมูล
+            $table->id(); // id ของตาราง pivot: ใช้เป็นรหัสหลักของตาราง
+            $table->foreignId('student_id')->constrained()->onDelete('cascade'); // foreign key สำหรับ student_id: เชื่อมโยงกับตาราง students และเมื่อข้อมูลใน students ถูกลบข้อมูลใน registers จะถูกลบอัตโนมัติ
+            $table->foreignId('course_id')->constrained()->onDelete('cascade'); // foreign key สำหรับ course_id: เชื่อมโยงกับตาราง courses และเมื่อข้อมูลใน courses ถูกลบข้อมูลใน registers จะถูกลบอัตโนมัติ
+            $table->timestamps(); // สำหรับการติดตามเวลาเมื่อมีการเพิ่มข้อมูล: สร้างคอลัมน์ created_at และ updated_at เพื่อเก็บเวลาที่บันทึกหรืออัพเดตข้อมูล
         });
     }
 
     /**
      * Reverse the migrations.
      *
-     * @return void
+     * ฟังก์ชันนี้ใช้ในการลบตาราง 'registers' ออกเมื่อย้อนกลับการทำ migration
      */
     public function down()
     {
-        Schema::dropIfExists('registers');
+        Schema::dropIfExists('registers'); // ลบตาราง registers ออก
     }
 }
